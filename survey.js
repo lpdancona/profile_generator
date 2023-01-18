38e1dee9a9b5ad70850e3376aa20bf4fcd5b28c4
Leonardo Picchi Dancona <lpdancona@gmail.com>
2023-01-17 17:35:50 -0800

    survey

---
+++ survey.js
@@ -0,0 +1,38 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question(
+  "What is your name? Nicknames are also acceptable 🙂 ",
+  (answer) => {
+    rl.question("What is an activity you like? ", (answer2) => {
+      rl.question("What do you listen to while doing that?", (answer3) => {
+        rl.question("Which meal is your favourite ?", (answer4) => {
+          rl.question(
+            "What's your favourite thing to eat for that meal?",
+            (answer5) => {
+              rl.question(
+                "Which sport is your absolute favourite?",
+                (answer6) => {
+                  rl.question(
+                    "What is your superpower? In a few words, tell us what you are amazing at!",
+                    (answer7) => {
+                      console.log(
+                        `${answer} loves listening to ${answer3} while ${answer2}, devouring ${answer5} for ${answer4}, prefers ${answer6} over any other sport, and is amazing in ${answer7}. `
+                      );
+
+                      rl.close();
+                    }
+                  );
+                }
+              );
+            }
+          );
+        });
+      });
+    });
+  }
+);
